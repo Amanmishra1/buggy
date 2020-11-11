8f7c10629355f3fd895330926fc784228212b380
Aman Mishra <amanmishra1@gmail.com>
2020-11-11 17:47:59 +0100

    update code

--- src/buggy.cpp
+++ src/buggy.cpp
@@ -1,62 +1,139 @@
-#include <thread>
-#include <vector>
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
+#include "buggy.h"
 
-struct Word
-{
-  char * data;
-  int count;
+std::mutex g_lockTST;
 
-  Word ( char * data_ ) :
-    data( ::strdup(data_) )
-  {}
-  
-  Word () :
-    data((char *)"")
-  {}
-};
-
-static std::vector<Word*> s_wordsArray;
+static TST s_wordsTST;
 static Word s_word;
 static int s_totalFound;
 
+TST::~TST()
+{
+  if (root_ != NULL)
+  {
+    del(root_);
+  }
+};
+
+void TST::del(Node *n)
+{
+  if (n == NULL)
+    return;
+  del(n->left);
+  del(n->mid);
+  del(n->right);
+  delete n;
+};
+
+Node *TST::put(Node *x, string key, int d)
+{
+  char c = key.at(d);
+  if (x == NULL)
+  {
+    x = new Node();
+    x->c = c;
+    x->count = 0;
+  }
+  if (c < x->c)
+    x->left = put(x->left, key, d);
+  else if (c > x->c)
+    x->right = put(x->right, key, d);
+  else if (d < key.length() - 1)
+    x->mid = put(x->mid, key, d + 1);
+  else
+    x->count++;
+  return x;
+}
+
+void TST::put(string key)
+{
+  root_ = put(root_, key, 0);
+}
+
+bool TST::contains(string key)
+{
+  return (get(key) != 0);
+}
+
+int TST::get(string key)
+{
+  Node *x = get(root_, key, 0);
+  if (x == NULL)
+    return 0;
+  return x->count;
+}
+
+Node *TST::get(Node *x, string key, int d)
+{
+  if (!x)
+    return x;
+  char c = key.at(d);
+  if (c < x->c)
+    return get(x->left, key, d);
+  else if (c > x->c)
+    return get(x->right, key, d);
+  else if (d < key.length() - 1)
+    return get(x->mid, key, d + 1);
+  else
+    return x;
+}
+
+int TST::printAll()
+{
+  queue<string> collectAll;
+  collect(root_, "", collectAll);
+  if (collectAll.empty())
+    return 0;
+  int size = collectAll.size();
+  while (collectAll.size() > 0)
+  {
+    //std::printf("\n %s " , collectAll->front());
+    std::cout << "\n  " << collectAll.front();
+    collectAll.pop();
+  }
+  printf("\n");
+  return size;
+}
+
+void TST::collect(Node *x, string prefix, queue<string> &q)
+{
+  if (x == NULL)
+    return;
+  char c = x->c;
+  ;
+  if (x->count > 0)
+    q.push(prefix + c);
+  collect(x->left, prefix, q);
+
+  collect(x->mid, prefix + c, q);
+
+  collect(x->right, prefix, q);
+}
 
 // Worker thread: consume words passed from the main thread and insert them
-// in the 'word list' (s_wordsArray), while removing duplicates. Terminate when
+// in the 'word list' (s_wordsTST), while removing duplicates. Terminate when
 // the word 'end' is encountered.
-static void workerThread ()
+static void workerThread()
 {
   bool endEncountered = false;
   bool found = false;
-  
+
   while (!endEncountered)
   {
     if (s_word.data[0]) // Do we have a new word?
     {
-      Word * w = new Word(s_word); // Copy the word
-      
+      Word *w = new Word(s_word); // Copy the word
+
       s_word.data[0] = 0; // Inform the producer that we consumed the word
-      
-      endEncountered = std::strcmp( s_word.data, "end" ) == 0;
-      
+
+      endEncountered = std::strcmp(w->data, "end") == 0;
+
       if (!endEncountered)
       {
-        // Do not insert duplicate words
-        for ( auto p : s_wordsArray )
-        {
-          if (!std::strcmp( p->data, w->data ))
-          {
-            ++p->count;
-            found = true;
-            break;
-          }
-        }
-
-        if (!found)
-          s_wordsArray.push_back( w );
+        g_lockTST.lock();
+        s_wordsTST.put(string(w->data));
+        g_lockTST.unlock();
       }
+      delete w;
     }
   }
 };
@@ -65,24 +142,26 @@ static void workerThread ()
 // inclusion in the word list.
 // Terminate when the word 'end' has been entered.
 //
-static void readInputWords ()
+static void readInputWords()
 {
   bool endEncountered = false;
-  
-  std::thread * worker = new std::thread( workerThread );
 
-  char * linebuf = new char[32];
-  
+  std::thread *worker = new std::thread(workerThread);
+
+  char *linebuf = new char[32];
+  s_word.data = new char[32];
+
   while (!endEncountered)
   {
-    if (!std::gets( linebuf )) // EOF?
+    if (!std::gets(linebuf)) // EOF?
       return;
-    
-    endEncountered = std::strcmp( linebuf, "end" ) == 0;
+
+    endEncountered = std::strcmp(linebuf, "end") == 0;
 
     // Pass the word to the worker thread
-    std::strcpy( s_word.data, linebuf );
-    while (s_word.data[0]); // Wait for the worker thread to consume it
+    std::strcpy(s_word.data, linebuf);
+    while (s_word.data[0])
+      ; // Wait for the worker thread to consume it
   }
 
   worker->join(); // Wait for the worker to terminate
@@ -91,65 +170,55 @@ static void readInputWords ()
 // Repeatedly ask the user for a word and check whether it was present in the word list
 // Terminate on EOF
 //
-static void lookupWords ()
+
+static void lookupWords()
 {
   bool found;
-  char * linebuf = new char[32];
-    
-  for(;;)
+  char *linebuf = new char[32];
+
+  for (;;)
   {
-    std::printf( "\nEnter a word for lookup:" );
-    if (std::scanf( "%s", linebuf ) == EOF)
+    std::printf("\nEnter a word for lookup:");
+    if (!std::gets(linebuf)) // EOF?
       return;
 
-    // Initialize the word to search for
-    Word * w = new Word();
-    std::strcpy( w->data, linebuf );
-
     // Search for the word
-    unsigned i;
-    for ( i = 0; i < s_wordsArray.size(); ++i )
-    {
-      if (std::strcmp( s_wordsArray[i]->data, w->data ) == 0)
-      {
-        found = true;
-        break;
-      }
-    }
+    int count = s_wordsTST.get(string(linebuf));
+    if (count > 0)
+      found = true;
 
     if (found)
     {
-      std::printf( "SUCCESS: '%s' was present %d times in the initial word list\n",
-                   s_wordsArray[i]->data, s_wordsArray[i]->count );
+      std::printf("SUCCESS: '%s' was present %d times in the initial word list\n",
+                  linebuf, count);
       ++s_totalFound;
     }
     else
-      std::printf( "'%s' was NOT found in the initial word list\n", w->data );
+      std::printf("'%s' was NOT found in the initial word list\n", linebuf);
   }
+
+  delete linebuf;
 }
 
-int main ()
+int main()
 {
   try
   {
+    std::printf("\n=== Provide list of words (List terminates when the word 'end' has been entered) :\n");
     readInputWords();
-    
-    // Sort the words alphabetically
-    std::sort( s_wordsArray.begin(), s_wordsArray.end() );
 
     // Print the word list
-    std::printf( "\n=== Word list:\n" );
-    for ( auto p : s_wordsArray )
-      std::printf( "%s %d\n", p->data, p->count );
+    std::printf("\n=== Word list:\n");
+    s_totalFound = s_wordsTST.printAll();
 
     lookupWords();
 
-    printf( "\n=== Total words found: %d\n", s_totalFound );
+    printf("\n=== Total words found: %d\n", s_totalFound);
   }
-  catch (std::exception & e)
+  catch (std::exception &e)
   {
-    std::printf( "error %s\n", e.what() );
+    std::printf("error %s\n", e.what());
   }
-  
+
   return 0;
 }
